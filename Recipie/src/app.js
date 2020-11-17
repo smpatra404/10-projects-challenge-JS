488f4ce3b84338bbc2db22106f7ac0bd2504af10
Subham <subham.mahapatra@capgemini.com>
2020-11-17 21:35:34 +0530

    Added homepage rendering. Made the card loading part into a function

--- Recipie/src/app.js
+++ Recipie/src/app.js
@@ -10,25 +10,40 @@ async function getRecipie(term) {
             return json_res.meals
         });
 }
-
+function render_recipiecards(recipie) {
+    let recipie_card = document.createElement('div');
+    recipie_card.setAttribute('class', 'recipie-card');
+    let card_image = document.createElement('img');
+    card_image.setAttribute('class', 'card-image');
+    card_image.setAttribute('src', recipie.strMealThumb);
+    card_image.setAttribute('alt', recipie.strMeal);
+    recipie_card.appendChild(card_image);
+    let card_name = document.createElement('div');
+    card_name.setAttribute('class', 'card-name');
+    card_name.innerText = recipie.strMeal;
+    recipie_card.appendChild(card_name);
+    recipiecards.appendChild(recipie_card);
+}
+async function homerender() {
+    const recipie_list = await getRecipie('');
+    document.querySelectorAll('.recipie-card').forEach(ele => {
+        ele.remove();
+    });
+    recipie_list.map((rec) => render_recipiecards(rec));
+}
 async function main() {
+    homerender();
     searchbtn.addEventListener('click', async () => {
-        let searchterm = searchbox.value;
+        const searchterm = searchbox.value;
         const recipie_list = await getRecipie(searchterm);
-        recipie_list.map((recipie) => {
-            let recipie_card = document.createElement('div');
-            recipie_card.setAttribute('class', 'recipie-card');
-            let card_image = document.createElement('img');
-            card_image.setAttribute('class', 'card-image');
-            card_image.setAttribute('src', recipie.strMealThumb);
-            card_image.setAttribute('alt', recipie.strMeal);
-            recipie_card.appendChild(card_image);
-            let card_name = document.createElement('div');
-            card_name.setAttribute('class', 'card-name');
-            card_name.innerText = recipie.strMeal;
-            recipie_card.appendChild(card_name);
-            recipiecards.appendChild(recipie_card);
+        document.querySelectorAll('.recipie-card').forEach(ele => {
+            ele.remove();
         });
+        if (recipie_list) {
+            recipie_list.map((rec) => render_recipiecards(rec));
+        } else {
+            alert('No result found for ' + searchterm);
+        }
     });
 }
 main()
