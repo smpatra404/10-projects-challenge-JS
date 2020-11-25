350da157a4a1b973ea7fd30bcb1459e281a1983a
Subham <smahapatra007@gmail.com>
2020-11-25 11:46:26 +0530

    Added functionality for removing task cards from home pade upon clicking the trash icon

--- To_Do/src/index.js
+++ To_Do/src/index.js
@@ -1,7 +1,45 @@
+const taskscontainer_element = document.querySelector('.taskcards-container');
+function getTasksLs() {
+    return JSON.parse((localStorage.getItem('task')));
+}
+let tasks_tracker = [];
+if (getTasksLs()) {
+    tasks_tracker = getTasksLs();
+}
+// render home page
+renderhome(tasks_tracker);
+function renderhome(taskslist) {
+    let count = 0;
+    taskslist.map((task) => {
+        let task_heading = '';
+        if (task.heading) {
+            task_heading = task.heading;
+        } else {
+            task_heading = "Unnamed Task";
+        }
+        let tasks_number = task.tasks.length;
+        let task_card = document.createElement('div');
+        task_card.className = 'todo-card';
+        task_card.id = count;
+        task_card.innerHTML = `<i class="fa fa-trash" aria-hidden="true"></i>
+                            <h3>${task_heading}</h3>
+                            <p>Number of tasks : ${tasks_number}</p>`;
+        taskscontainer_element.appendChild(task_card);
+        count += 1;
+    });
+}
+// deleting task cards from home page
+document.querySelectorAll('.todo-card i').forEach((t) => {
+    t.addEventListener('click', () => {
+        tasks_tracker.splice(t.parentElement.id, 1);
+        localStorage.setItem('task', JSON.stringify(tasks_tracker));
+        location.reload();
+    });
+});
+
 // handling grid/row view
 const gridview_element = document.querySelector('#grid-view');
 const rowview_element = document.querySelector('#row-view');
-const taskscontainer_element = document.querySelector('.taskcards-container');
 //grid
 gridview_element.addEventListener('click', () => {
     taskscontainer_element.classList.remove('rowview');
@@ -56,16 +94,16 @@ taskname_element.addEventListener('input', () => {
 });
 // entring the task to the list on clicking the enter key
 taskname_element.addEventListener('keyup', (e) => {
-    if (task_value) {
-        if (e.keyCode === 13) {
+    if (e.keyCode === 13) {
+        if (task_value) {
             e.preventDefault();
             addtask(task_value);
             tasks_list.push(task_value);
             taskname_element.value = '';
             task_value = '';
+        } else {
+            alert('Invalid task entered !!!')
         }
-    } else {
-        alert('Invalid task entered !!!')
     }
 });
 // entering the task to the list after clicking on green tick
@@ -88,20 +126,26 @@ function addtask(task) {
     tasklist_element.insertAdjacentElement('afterbegin', task_li);
 }
 // saving tasks list
-let tracker = [];
 const savetask_element = document.querySelector('#save-task');
 savetask_element.addEventListener('click', () => {
-    tracker.push({
-        heading: taskheading_value,
-        tasks: tasks_list
-    });
-    localStorage.setItem('task', JSON.stringify(tracker));
-    tasks_list = [];
-    taskheading_element.value = '';
-    taskname_element.value = '';
-    tasklist_element.querySelectorAll('li').forEach((li) => {
-        li.remove();
-    });
+    if (tasks_list.length > 0) {
+        tasks_tracker.push({
+            heading: taskheading_value,
+            tasks: tasks_list
+        });
+        localStorage.setItem('task', JSON.stringify(tasks_tracker));
+        renderhome([{ heading: taskheading_value, tasks: tasks_list }]);
+        task_value = '';
+        tasks_list = [];
+        taskheading_element.value = '';
+        taskname_element.value = '';
+        tasklist_element.querySelectorAll('li').forEach((li) => {
+            li.remove();
+        });
+        alert('Added successfully !!!');
+    } else {
+        alert('At least one task need to be entered');
+    }
 });
 // discarding
 const deletetask_element = document.querySelector('#delete-task');
@@ -115,4 +159,3 @@ deletetask_element.addEventListener('click', () => {
         li.remove();
     });
 });
-
