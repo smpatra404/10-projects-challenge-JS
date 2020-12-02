ec44237e7f9136694d081b763f1a15fb938eb98b
Subham <smahapatra007@gmail.com>
2020-12-02 17:34:26 +0530

    Added functions to get woeid and temp. of city

--- Weather/src/index.js
+++ Weather/src/index.js
@@ -1,12 +1,40 @@
 const woid_url = 'https://www.metaweather.com/api/location/search/?query=';
-async function fetchid() {
-    const res = await fetch('https://www.metaweather.com/api/location/search/?query=pune')
+const temp_url = 'https://www.metaweather.com/api/location/'
+async function fetchid(city) {
+    const res = await fetch('https://www.metaweather.com/api/location/search/?query=' + city)
         .then(res => res.json());
-
-    return res;
+    if (res.length > 0) {
+        return res;
+    }
+    else {
+        return undefined;
+    }
 }
-async function main() {
-    const d = await fetchid();
-    console.log(d)
+async function fetchtemp(city) {
+    const id = await fetchid(city);
+    if (id) {
+        const temp = await fetch('https://www.metaweather.com/api/location/' + id[0].woeid)
+            .then(res => res.json());
+        return {
+            temp: temp.consolidated_weather[0].the_temp,
+            city: temp.title,
+        };
+    } else {
+        return undefined;
+    }
 }
-main()
+function main() {
+    const weatherdiv_element = document.querySelector('.weather-detail');
+    const searchbar_element = document.querySelector('#search-box');
+    const searchbtn_element = document.querySelector('#search-btn');
+    searchbtn_element.addEventListener('click', async () => {
+        let res = await fetchtemp(searchbar_element.value);
+        if (res) {
+            weatherdiv_element.innerHTML = `<h1> ${res.city} </h1> <h3> ${res.temp} °C</h3>`;
+        } else {
+            alert('No results found, Try something else.');
+        }
+        searchbar_element.value = '';
+    });
+}
+main();
