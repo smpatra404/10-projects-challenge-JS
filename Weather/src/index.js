e8b676697c38490b95fb32c70ef6374b1477d73d
Subham <smahapatra007@gmail.com>
2020-12-02 17:53:06 +0530

    Added function for weather icon

--- Weather/src/index.js
+++ Weather/src/index.js
@@ -18,6 +18,7 @@ async function fetchtemp(city) {
         return {
             temp: temp.consolidated_weather[0].the_temp,
             city: temp.title,
+            icon: temp.consolidated_weather[0].weather_state_abbr,
         };
     } else {
         return undefined;
@@ -27,9 +28,11 @@ function main() {
     const weatherdiv_element = document.querySelector('.weather-detail');
     const searchbar_element = document.querySelector('#search-box');
     const searchbtn_element = document.querySelector('#search-btn');
+    const weathericon_element = document.querySelector('#weather-icon');
     searchbtn_element.addEventListener('click', async () => {
         let res = await fetchtemp(searchbar_element.value);
         if (res) {
+            weathericon_element.setAttribute('src', 'https://www.metaweather.com/static/img/weather/' + res.icon + '.svg');
             weatherdiv_element.innerHTML = `<h1> ${res.city} </h1> <h3> ${res.temp} °C</h3>`;
         } else {
             alert('No results found, Try something else.');
